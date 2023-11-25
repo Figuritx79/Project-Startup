8f705a3fa4fa06e8f0f7180f0c2cf70469748c12
Enrique Gonzalez <gonherenrique@gmail.com>
2023-11-25 03:33:42 -0600

    Inferior y superior agregado, Despligue listo

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,9 @@ const router = Router();
 router.get("/", (req, res) =>
   res.render("index", { title: "My Web with express" })
 );
-router.get("/about", (req, res) => res.render("about"));
 router.get("/login", (req, res) => res.render("login"));
 router.get("/accesorios", (req, res) => res.render("accesorios"));
+router.get("/inferior", (req, res) => res.render("inferior"));
+router.get("/superior", (req, res) => res.render("superior"));
 
 export default router;
